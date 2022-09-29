c4b62d09c948e6c2ea2a2eb5222a23a2dbc0ed0d
yana.radeva7 <yana.radeva7@gmail.com>
2022-09-30 00:07:14 +0300

    update app component and app module

--- workshop-forum/forum/src/app/app.module.ts
+++ workshop-forum/forum/src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ThemeListComponent } from './theme-list/theme-list.component';
+import { AsideComponent } from './aside/aside.component';
+import { ThemeListItemComponent } from './theme-list-item/theme-list-item.component';
+import { UserService } from './user.service';
+import { storageServiceProvider } from './storage.service';
+import { ThemeService } from './theme.service';
+import { PostService } from './post.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ThemeListComponent,
+    AsideComponent,
+    ThemeListItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: UserService,
+      useClass: UserService
+    },
+    storageServiceProvider,
+    ThemeService,
+    PostService
+  ],
+  bootstrap: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ]
 })
 export class AppModule { }
