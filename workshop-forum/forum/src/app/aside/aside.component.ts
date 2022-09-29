100b3470574d963f4ad916d09a70e634759df723
yana.radeva7 <yana.radeva7@gmail.com>
2022-09-30 00:02:28 +0300

    create header, footer and aside component

---
+++ workshop-forum/forum/src/app/aside/aside.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { IPost } from '../interfaces';
+import { PostService } from '../post.service';
+
+@Component({
+  selector: 'app-aside',
+  templateUrl: './aside.component.html',
+  styleUrls: ['./aside.component.css']
+})
+export class AsideComponent implements OnInit {
+
+  postList: IPost[] = [];
+
+  constructor(private postService: PostService) { }
+
+  ngOnInit(): void {
+    this.postService.loadPostList$(5).subscribe(posts => {
+      this.postList = posts;
+    });
+  }
+
+}
