0d7d17c0d5db7da279d6e7d64f2dbf2ffc85c0c4
yana.radeva7 <yana.radeva7@gmail.com>
2022-09-30 21:15:55 +0300

    add pages module

---
+++ workshop-forum/forum/src/app/feature/pages/pages.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { PageNotFoundPageComponent } from './page-not-found-page/page-not-found-page.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    HomePageComponent,
+    PageNotFoundPageComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule
+  ],
+  exports: [
+    HomePageComponent
+  ]
+})
+export class PagesModule { }
