520dea10ab3da1bc97c5c663b38c94e074d0e09e
yana.radeva7 <yana.radeva7@gmail.com>
2022-10-04 21:34:55 +0300

    create login reactive form

--- workshop-forum/forum/src/app/auth/login/login.component.ts
+++ workshop-forum/forum/src/app/auth/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { tap } from 'rxjs';
 import { UserService } from 'src/app/core/user.service';
+import { emailValidator } from '../util';
 
 @Component({
   selector: 'app-login',
@@ -8,17 +11,39 @@ import { UserService } from 'src/app/core/user.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  errorMessage: string = '';
+
+  loginFormGroup: FormGroup = this.formBuilder.group({
+    'email': new FormControl('', [Validators.required, emailValidator]), //Validators.pattern 
+    'password': new FormControl('', [Validators.required, Validators.minLength(5)])
+  });
 
   constructor(
     private userService: UserService,
-    private router: Router) { }
+    private router: Router,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
-  loginHandler(): void {
+  handleLogin(): void {
     // TODO: validate users's data
-    this.userService.login();
-    this.router.navigate(['/home']);
+    // this.userService.login();
+    // this.router.navigate(['/home']);
+
+    this.errorMessage = '';
+
+    this.userService.login$(this.loginFormGroup.value).subscribe({      
+      next: (user) => {
+        console.log(user);
+        this.router.navigate(['/home'])
+      },
+      complete: () => {
+        console.log('login stream completed');
+      },
+      error: (err) => {
+        this.errorMessage = err.error.message;
+      }
+    });
   }
 }
