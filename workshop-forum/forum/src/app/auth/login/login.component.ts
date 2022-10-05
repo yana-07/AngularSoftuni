6503be9cb764808ba4db1b4339d0fec90bf08849
yana.radeva7 <yana.radeva7@gmail.com>
2022-10-05 10:53:19 +0300

    add comments for demo purposes on differences between ngSubmit and onSubmit

--- workshop-forum/forum/src/app/auth/login/login.component.ts
+++ workshop-forum/forum/src/app/auth/login/login.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { tap } from 'rxjs';
 import { UserService } from 'src/app/core/user.service';
 import { emailValidator } from '../util';
+
+const myRequired = (control: AbstractControl): ValidationErrors | null => {
+  console.log('validator called');
+  return Validators.required(control);
+}
 
 @Component({
   selector: 'app-login',
@@ -14,7 +19,10 @@ export class LoginComponent implements OnInit {
   errorMessage: string = '';
 
   loginFormGroup: FormGroup = this.formBuilder.group({
-    'email': new FormControl('', [Validators.required, emailValidator]), //Validators.pattern 
+    'email': new FormControl('', {
+       validators: [Validators.required, emailValidator], //Validators.pattern 
+       //updateOn: 'submit'
+      }), 
     'password': new FormControl('', [Validators.required, Validators.minLength(5)])
   });
 
@@ -24,12 +32,21 @@ export class LoginComponent implements OnInit {
     private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    // this.loginFormGroup.get('email')?.valueChanges.subscribe(value =>{
+    //   console.log(value)
+    // });
   }
+
+  // loginHandler() {
+  //   console.log('from click handler')
+  // }
 
   handleLogin(): void {
     // TODO: validate users's data
     // this.userService.login();
     // this.router.navigate(['/home']);
+
+    //console.log('from ngSubmit');
 
     this.errorMessage = '';
 
