30d11d5f2b290f7c77d12142d43bcc4fd17223f2
yana.radeva7 <yana.radeva7@gmail.com>
2022-10-06 16:55:17 +0300

    implement search for theme list

--- workshop-forum/forum/src/app/core/theme.service.ts
+++ workshop-forum/forum/src/app/core/theme.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpParams } from '@angular/common/http'
 import { IPost, ITheme, IUser } from './interfaces/index';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -15,8 +15,13 @@ export class ThemeService {
     return this.http.post<ITheme>(`${environment.apiUrl}/themes`, body, { withCredentials: true });
   }
 
-  loadThemeList$(): Observable<ITheme[]> {
-    return this.http.get<ITheme[]>(`${apiUrl}/themes`); 
+  loadThemeList$(searchTerm: string = ''): Observable<ITheme[]> {
+    return this.http.get<ITheme[]>(`${apiUrl}/themes?title=${searchTerm}`, { // or ?title={searchTerm}
+      params: new HttpParams({
+        fromObject: {         
+        }
+      })
+    }); 
   }
 
   loadThemeById$(id: string): Observable<ITheme<IPost>> {
--- workshop-forum/forum/src/app/feature/themes/theme-list/theme-list.component.ts
+++ workshop-forum/forum/src/app/feature/themes/theme-list/theme-list.component.ts
@@ -1,4 +1,6 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, filter, startWith, switchMap } from 'rxjs';
 import { ITheme } from '../../../core/interfaces';
 import { ThemeService } from '../../../core/theme.service';
 
@@ -8,15 +10,36 @@ import { ThemeService } from '../../../core/theme.service';
   styleUrls: ['./theme-list.component.css']
 })
 export class ThemeListComponent implements OnInit, AfterViewInit {
+  l10n = {
+    'en': {
+      'no-themes-message': 'No themes!'
+    },
+    'bg': {
+      'no-themes-message': 'Няма теми!'
+    }
+  }
+
+  localize(key: string, l10n: {[key: string]: {[key: string]: string}}) {
+    const local = 'bg';
+
+    return l10n[local][key];
+  }
 
   themeList: ITheme[] = [];
+
+  searchControl = new FormControl(''); // { updateOn: 'blur' }
 
   constructor(private themeService: ThemeService) { }
 
   ngOnInit(): void {
-    this.themeService.loadThemeList$().subscribe(themeList => {
-      this.themeList = themeList;
-    });
+    this.searchControl.valueChanges.pipe(
+      debounceTime(300), // e.g. starting with string 'a' it waits 300 ms and in case of no new input, emits 'a', otherwise it waits for another 300 ms, during the time 'n' is typed, so it waits for another 300 ms and in case of no new input, it emits 'an'
+      filter(searchTerm => searchTerm!.length >= 3),
+      startWith(''),
+      switchMap(searchTerm => this.themeService.loadThemeList$(searchTerm as any)))
+      .subscribe(themeList => {
+        this.themeList = themeList;
+      });
   }
 
   ngAfterViewInit(): void {
