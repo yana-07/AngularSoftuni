e1edfc5215c35b5e4aa36b1bb03bdcdaafcf6178
yana.radeva7 <yana.radeva7@gmail.com>
2022-10-16 10:27:02 +0300

    implement pagination with search terms

--- workshop-forum/forum/src/app/feature/themes/theme-list/theme-list.component.ts
+++ workshop-forum/forum/src/app/feature/themes/theme-list/theme-list.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { debounceTime, filter, startWith, switchMap } from 'rxjs';
+import { BehaviorSubject, combineLatest, debounceTime, filter, startWith, switchMap, tap } from 'rxjs';
 import { ITheme } from '../../../core/interfaces';
 import { ThemeService } from '../../../core/theme.service';
 
@@ -25,25 +25,50 @@ export class ThemeListComponent implements OnInit, AfterViewInit {
     return l10n[local][key];
   }
 
+  private pageChanged$ = new BehaviorSubject(undefined);
+
   themeList: ITheme[] = [];
+
+  readonly pageSize = 4;
+  currentPage: number = 0;
+  totalResults: number = 0;
 
   searchControl = new FormControl(''); // { updateOn: 'blur' }
 
   constructor(private themeService: ThemeService) { }
 
   ngOnInit(): void {
-    this.searchControl.valueChanges.pipe(
-      debounceTime(300), // e.g. starting with string 'a' it waits 300 ms and in case of no new input, emits 'a', otherwise it waits for another 300 ms, during the time 'n' is typed, so it waits for another 300 ms and in case of no new input, it emits 'an'
-      filter(searchTerm => searchTerm!.length >= 3),
-      startWith(''),
-      switchMap(searchTerm => this.themeService.loadThemeList$(searchTerm as any)))
+    combineLatest([
+      this.searchControl.valueChanges.pipe(
+        debounceTime(300), // e.g. starting with string 'a' it waits 300 ms and in case of no new input, emits 'a', otherwise it waits for another 300 ms, during the time 'n' is typed, so it waits for another 300 ms and in case of no new input, it emits 'an'
+        //filter(searchTerm => searchTerm!.length >= 3),
+        startWith(''),
+        //tap(() => this.currentPage = 0)
+      ),
+      this.pageChanged$
+    ])
+      .pipe( // a new request will be made each time the search term or the page changes
+        switchMap(([searchTerm]) => this.themeService
+          .loadThemePaginatedList$(searchTerm as any, this.currentPage * this.pageSize, this.pageSize))
+      )
       .subscribe(themeList => {
-        this.themeList = themeList;
+        this.totalResults = themeList.totalResults;
+        this.themeList = themeList.results;
       });
   }
 
   ngAfterViewInit(): void {
     console.log('View was initialized');
+  }
+
+  goOnePageBack(): void {
+    this.currentPage--;
+    this.pageChanged$.next(undefined);
+  }
+
+  goOnePageForward(): void {
+    this.currentPage++;
+    this.pageChanged$.next(undefined);
   }
 
 }
